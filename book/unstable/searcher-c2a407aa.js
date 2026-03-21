509999a30c491958c336cefc98590e5d082ae154
teal-language <hisham@gobolinux.org>
2026-03-21 23:42:14 +0000

    Update from https://github.com/teal-language/tl/commit/c8882c65dbb736012731edf9d165a4a253f613a7

--- book/unstable/searcher-c2a407aa.js
+++ book/unstable/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a8644d9b.js',
+                path_to_root + 'searchindex-a322c924.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
